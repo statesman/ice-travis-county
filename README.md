3d137fa62a3ff81a83e6579a06afa1269ab4e82f
Cody Winchester <cjwinchester@gmail.com>
2016-10-06 12:29:47 -0500

    jupyter notebook

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-ice-travis-county
+# ICE detainers in Travis County
+
+![ice ice baby](https://media.giphy.com/media/j0A3pPBp0NAXe/giphy.gif)
+
+(not even sorry)
+
+This repo contains a Jupyter notebook with scripts to process and analyze ICE detainer data in Travis County, Texas, from the mid-2000s to October 2016.
+
+### Requirements
+* python
+* virtualenv/virtualenvwrapper
+
+### Setup
+Clone or DL this repo, then:
+```shell
+$ cd ice-travis-county
+$ mkvirtualenv ice-data
+$ pip install -r requirements.txt
+$ jupyter notebook ice-data-parser.ipynb
+```
+
+### Assumptions and issues
+Some of the booking dates go back to the mid-90s, which is interesting, because ICE wasn't formed until 2003 or so. According to XXXX, data before XXX is unreliable, so records on or before XXXXX are excluded from this analysis.
+
+For the purposes of tallying detainees' self-reported countries of origin, we classified the Federated States of Micronesia (a U.S. "associated state" with its own U.N. representative) as its own country. Others we grouped with a "parent" country:
+* British Indian Ocean Territory (United Kingdom)
+* French Southern and Antartic [_sic_] Lands (France)
+* Guadeloupe (France)
+* Guam (United States)
+* Hong Kong (China)
+* Howland Island (United States)
+* Juan de Nova Island (France)
+* Northern Mariana Islands (United States)
+* Puerto Rico (United States)
+* Tokelau (New Zealand)
+* Virgin Islands (United States)
+
+(If any of these assumptions are unwarranted, please let us know.)
---
+++ text_transforms.py
@@ -0,0 +1,154 @@
+"""
+Country records that don't need transformed:
+    Albania
+    Argentina
+    Bangledesh
+    Barbados
+    Belize
+    Bosnia and Herzegovina
+    Brazil
+    Burma
+    Burundi
+    Cameroon
+    Canada
+    Costa Rica
+    Dominican Republic
+    Ecuador
+    Egypt
+    El Salvador
+    Eritrea
+    Ethiopia
+    Federated States of Micronesia
+    France
+    Gabon
+    Grenada
+    Guatemala
+    Honduras
+    Hungary
+    Iceland
+    India
+    Indonesia
+    Iran
+    Iraq
+    Ireland
+    Ivory Coast
+    Jamaica
+    Japan
+    Jordan
+    Kenya
+    Mauritania
+    Morocco
+    Nepal
+    Nicaragua
+    Niger
+    Nigeria
+    North Korea
+    Other
+    Pakistan
+    Panama
+    Peru
+    Philippines
+    Romania
+    Russia
+    Saudi Arabia
+    Scotland
+    Senegal
+    South Korea
+    Spain
+    Sudan
+    Surinam
+    Sweden
+    Thailand
+    Togo
+    Tunisia
+    Turkey
+    Ukraine
+    Vanuatu
+    Yugoslavia
+"""
+
+COUNTRIES = (
+    ('Abuascalientes, Mexico', 'Mexico'),
+    ('Africa-Reference Only', 'Africa, unspecified'),
+    ('Alabama', 'United States'),
+    ('Australia (American Samoa for boat RES)', 'Australia'),
+    ('Bahrain/Bahrein', 'Bahrain'),
+    ('Baja California (Northern), Mexico', 'Mexico'),
+    ('British Indian Ocean Territory', 'United Kingdom'),
+    ('California (Not for boat RES)', 'United States'),
+    ('Campeche, Mexico', 'Mexico'),
+    ('Chiapas, Mexico', 'Mexico'),
+    ('Chihuahua, Mexico', 'Mexico'),
+    ('China-Reference only', 'China'),
+    ('Coahuila, Mexico', 'Mexico'),
+    ('Colombia, Republic of', 'Colombia'),
+    ('Congo-Reference only', 'Congo'),
+    ('Cuba, Republic of', 'Cuba'),
+    ('Distrito Federal, Mexico', 'Mexico'),
+    ('Durango, Mexico', 'Mexico'),
+    ('England', 'United Kingdom'),
+    ('Florida', 'United States'),
+    ('Foreign country not listed', 'Other'),
+    ('French Southern and Antartic Lands', 'France'),
+    ('Germany (East and West)', 'Germany'),
+    ('Guadeloupe', 'France'),
+    ('Guam', 'United States'),
+    ('Guanajuato, Mexico', 'Mexico'),
+    ('Guerrero, Mexico', 'Mexico'),
+    ('Hidalgo, Mexico', 'Mexico'),
+    ('Hong Kong', 'China'),
+    ('Howland Island', 'United States'),
+    ('Illinois', 'United States'),
+    ('Indiana', 'U.S.A'),
+    ('Italy, includes Sicily and Sardinia', 'Italy'),
+    ('Jalisco, Mexico', 'Mexico'),
+    ('Juan de Nova Island', 'France'),
+    ('Kansas (not for boat RES-see KA)', 'United States'),
+    ('Kansas-boat RES only', 'United States'),
+    ('Louisiana', 'United States'),
+    ('Maine', 'United States'),
+    ('Maryland', 'United States'),
+    ('Massachusetts (not for boat RES', 'United States'),
+    ('Mexico (State)', 'Mexico'),
+    ('Mexico (Use only when state is unknown)', 'Mexico'),
+    ('Michigan-not for boat RES (boat RES 4 MS)', 'United States'),
+    ('Michoacan, Mexico (boat RES for MI)', 'Mexico'),
+    ('Morelos, Mexico', 'Mexico'),
+    ('Nayarit, Mexico', 'Mexico'),
+    ('New Jersey', 'United States'),
+    ('New Mexico', 'United States'),
+    ('New York', 'United States'),
+    ('North Carolina', 'United States'),
+    ('Northern Mariana Islands', 'United States'),
+    ('Nuevo Leon, Mexico', 'Mexico'),
+    ('Oaxaca, Mexico', 'Mexico'),
+    ("People's Republic of China", 'China'),
+    ('Puebla, Mexico', 'Mexico'),
+    ('Puerto Rico', 'United States'),
+    ('Quebec, Canada', 'Canada'),
+    ('Queretaro, Mexico', 'Mexico'),
+    ('San Luis Potosi, Mexico', 'Mexico'),
+    ('Sinaloa, Mexico', 'Mexico'),
+    ('Socialist Republic of Vietname (North VN)', 'Vietnam'),
+    ('Sonora, Mexico', 'Mexico'),
+    ('South Vietnam-Reference Only', 'Vietnam'),
+    ('Tabasco, Mexico', 'Mexico'),
+    ('Taiwan, Republic of China', 'Taiwan'),
+    ('Tamaulipas, Mexico', 'Mexico'),
+    ('Texas', 'United States'),
+    ('Tlaxcala, Mexico', 'Mexico'),
+    ('Tokelau', 'New Zealand'),
+    ('Venezuela, Republic of', 'Venezuela'),
+    ('Veracruz, Mexico', 'Mexico'),
+    ('Vietnam-Reference Only', 'Vietnam'),
+    ('Virgin Islands', 'United States'),
+    ('Wisconsin - not for boat RES', 'United States'),
+    ('Zacatecas, Mexico', 'Mexico'),
+    ('Zambia, Republic of', 'Zambia')
+)
+
+"""
+CHARGE_LEVELS = (
+    ()
+)
+"""
